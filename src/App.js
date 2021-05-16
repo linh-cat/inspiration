4bebaa09946a2e8cc07bddec09ad337944a8370e
Nguyen Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2021-05-16 22:48:25 +0700

    add all file

--- src/App.js
+++ src/App.js
@@ -20,6 +20,27 @@ function App() {
       <div className="gallery__recommend">
         <Row title="Gallery Recommends" />
       </div>
+      <div className="button__group">
+        <div className="icon">
+          <div className="icon__player">
+            <img alt="player" src="/player.png" />
+          </div>
+          <div className="icon__group">
+            <div className="icon__group--view">
+              <img src="/svg_widget_seen.png" alt="seen" />
+              <p>999999</p>
+            </div>
+            <div className="icon__group--like">
+              <img src="/svg_widget_like.png" alt="like" />
+              <p>999000</p>
+            </div>
+            <div className="icon__group--share">
+              <img src="/svg_widget_share.png" alt="share" />
+              <p>999903</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
